a752f014b266e138c55897b353bfe94417ffedfb
Ian Speers <ianmspeers@gmail.com>
2022-09-23 08:48:40 -0400

    Add hunspell, paste script

--- index.js
+++ index.js
@@ -7,3 +7,36 @@ const affix = fs.readFileSync(constants.AFFIX_PATH).toString();
 
 
 console.log(dictionary, '\n\n', affix);
+
+
+
+
+const Spellchecker = require("hunspell-spellchecker")
+
+const spellchecker = new Spellchecker();
+
+const DICT = spellchecker.parse({
+    dic:
+`2
+microsoft.com
+gmail.com`,
+    aff:
+`SET UTF-8
+
+REP 2
+REP co com
+REP com co`,
+});
+
+spellchecker.use(DICT);
+
+const check = (word) => {
+    if (!spellchecker.check(word)) {
+        console.log(`${word} NOT OK, DID YOU MEAN [${spellchecker.suggest(word).join(', ')}]`);
+    } else{
+        console.log(`${word} OK`);
+    }
+}
+
+check('gmail.com');
+check('gmail.co');
